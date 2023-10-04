c9bc4bcc02ee69e69c2d254c9e55fe9dc15a77ae
dipenmotka <dipen@atharvasystem.com>
2023-10-04 15:43:13 +0530

    working on pnl data

--- src/app/modules/pages/dashboard/dashboard.component.ts
+++ src/app/modules/pages/dashboard/dashboard.component.ts
@@ -9,109 +9,108 @@ import { IStrategy } from 'src/app/core/models/stretag.model';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule,ClockComponent,StretagyboxComponent],
+  imports: [CommonModule, ClockComponent, StretagyboxComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit,OnDestroy {
-  subs:Subscription[] = [];
-  localData:string = '';
-  ticks:any;
+export class DashboardComponent implements OnInit, OnDestroy {
+  subs: Subscription[] = [];
+  localData: string = '';
+  ticks: any;
   niftyTicker = 0;
   bankniftyTicker = 0;
-  strategies:IStrategy[] = [];
+  strategies: IStrategy[] = [];
+  accounts: any[] = [];
 
-  constructor(private api:ApiCallService){
-    this.strategies = [
-      {
-        name:'STG 1',
-        value: '+20,000'
-      },
-      {
-        name:'STG 2',
-        value: '-10,000'
-      },
-      {
-        name:'STG 3',
-        value: '+25,000'
-      },
-      {
-        name:'STG 4',
-        value: '+21,800'
-      },
-      {
-        name:'STG 5',
-        value: '-18,000'
-      },
-      {
-        name:'STG 6',
-        value: '-11,000'
-      },
-      {
-        name:'STG 7',
-        value: '+23,000'
-      },
-      {
-        name:'STG 8',
-        value: '+19,000'
-      },
-      {
-        name:'STG 9',
-        value: '-1,000'
-      },
-      {
-        name:'STG 10',
-        value: '+20,000'
-      },
-      {
-        name:'STG 11',
-        value: '+20,500'
-      },
-      {
-        name:'STG 12',
-        value: '+20,800'
-      },
-      {
-        name:'STG 13',
-        value: '+32,000'
-      },
-      {
-        name:'STG 14',
-        value: '+50,000'
-      },
-      {
-        name:'STG 15',
-        value: '-3,000'
-      },
-    ]
+  constructor(private api: ApiCallService) {
+    // this.strategies = [
+    //   {
+    //     name: 'STG 1',
+    //     value: '+20,000'
+    //   },
+    //   {
+    //     name: 'STG 2',
+    //     value: '-10,000'
+    //   },
+    //   {
+    //     name: 'STG 3',
+    //     value: '+25,000'
+    //   },
+    //   {
+    //     name: 'STG 4',
+    //     value: '+21,800'
+    //   },
+    //   {
+    //     name: 'STG 5',
+    //     value: '-18,000'
+    //   },
+    //   {
+    //     name: 'STG 6',
+    //     value: '-11,000'
+    //   },
+    //   {
+    //     name: 'STG 7',
+    //     value: '+23,000'
+    //   },
+    //   {
+    //     name: 'STG 8',
+    //     value: '+19,000'
+    //   },
+    //   {
+    //     name: 'STG 9',
+    //     value: '-1,000'
+    //   },
+    //   {
+    //     name: 'STG 10',
+    //     value: '+20,000'
+    //   },
+    //   {
+    //     name: 'STG 11',
+    //     value: '+20,500'
+    //   },
+    //   {
+    //     name: 'STG 12',
+    //     value: '+20,800'
+    //   },
+    //   {
+    //     name: 'STG 13',
+    //     value: '+32,000'
+    //   },
+    //   {
+    //     name: 'STG 14',
+    //     value: '+50,000'
+    //   },
+    //   {
+    //     name: 'STG 15',
+    //     value: '-3,000'
+    //   },
+    // ]
   }
 
   ngOnInit(): void {
-      this.subs.push(
-        this.api.getInitialData().subscribe(data=>{
-          this.ticks = data;
-          if(this.ticks['nifty'] != undefined)
-            this.niftyTicker = this.ticks['nifty'];
+    this.subs.push(
+      this.api.getN_BnTicker().subscribe(data => {
+        this.ticks = data;
+        if (this.ticks['nifty'] != undefined)
+          this.niftyTicker = this.ticks['nifty'];
 
-          if(this.ticks['bank_nifty'] != undefined)
-            this.bankniftyTicker = this.ticks['bank_nifty'];
+        if (this.ticks['bank_nifty'] != undefined)
+          this.bankniftyTicker = this.ticks['bank_nifty'];
+      })
+    );
+
+    this.accounts.push(
+      this.api.getPnLData().subscribe((data: any) => {
+        this.strategies =  data.map((account: any) => {
+          name: account.fullname;
+          value: account.total;
         })
-      );
-
-      this.subs.push(
-        this.api.getUpdatedData().subscribe(data=>{
-          this.ticks = data;
-          if(this.ticks['nifty'] != undefined)
-            this.niftyTicker = this.ticks['nifty'];
-
-          if(this.ticks['bank_nifty'] != undefined)
-            this.bankniftyTicker = this.ticks['bank_nifty'];
-        })
-      )
+      })
+    )
   }
 
   ngOnDestroy(): void {
-      this.subs.forEach(subscription => { subscription.unsubscribe() });
+    this.subs.forEach(subscription => { subscription.unsubscribe() });
   }
 
 }
